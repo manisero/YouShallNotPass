a2c20cce8a65be99de70e03320d4c36d44d5ecc1
manisero <michalaniserowicz@gmail.com>
2017-11-01 16:56:43 +0100

    Finished 'custom validations' sample

---
+++ src/Manisero.YouShallNotPass.Samples/Custom_validations/Macro_rules___inheritance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Manisero.YouShallNotPass.Core.Engine;
+using Manisero.YouShallNotPass.Validations;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Custom_validations
+{
+    public class Macro_rules___inheritance
+    {
+        // TODO: Currently this scenario is not supported (test will fail). Consider supporting it.
+
+        // Password validation
+
+        public class PasswordValidationRule : AtLeastNValidationRule<string>
+        {
+            private static readonly ICollection<IValidationRule<string>> _rules = new List<IValidationRule<string>>
+            {
+                new ContainsDigitValidationRule(),
+                new ContainsLowerLetterValidationRule(),
+                new ContainsUpperLetterValidationRule()
+            };
+
+            public PasswordValidationRule()
+            {
+                Rules = _rules;
+                N = 2;
+            }
+        }
+
+        [Theory]
+        [InlineData("1aA", true)]
+        [InlineData("1aa", true)]
+        [InlineData("aaA", true)]
+        [InlineData("aaa", false)]
+        [InlineData("111", false)]
+        [InlineData("...", false)]
+        public void sample(string password, bool isValid)
+        {
+            var builder = new ValidationEngineBuilder();
+            builder.RegisterValidator(new ContainsDigitValidator());
+            builder.RegisterValidator(new ContainsLowerLetterValidator());
+            builder.RegisterValidator(new ContainsUpperLetterValidator());
+
+            var engine = builder.Build();
+
+            var rule = new PasswordValidationRule();
+
+            var validResult = engine.Validate(password, rule);
+
+            validResult.HasError().Should().Be(!isValid);
+        }
+    }
+}
---
+++ src/Manisero.YouShallNotPass.Samples/Custom_validations/Macro_rules___instance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Manisero.YouShallNotPass.Core.Engine;
+using Manisero.YouShallNotPass.Validations;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Custom_validations
+{
+    public class Macro_rules___instance
+    {
+        private static readonly AtLeastNValidationRule<string> PasswordValidationRule = new AtLeastNValidationRule<string>
+        {
+            Rules = new List<IValidationRule<string>>
+            {
+                new ContainsDigitValidationRule(),
+                new ContainsLowerLetterValidationRule(),
+                new ContainsUpperLetterValidationRule()
+            },
+            N = 2
+        };
+
+        [Theory]
+        [InlineData("1aA", true)]
+        [InlineData("1aa", true)]
+        [InlineData("aaA", true)]
+        [InlineData("aaa", false)]
+        [InlineData("111", false)]
+        [InlineData("...", false)]
+        public void sample(string password, bool isValid)
+        {
+            var builder = new ValidationEngineBuilder();
+            builder.RegisterValidator(new ContainsDigitValidator());
+            builder.RegisterValidator(new ContainsLowerLetterValidator());
+            builder.RegisterValidator(new ContainsUpperLetterValidator());
+
+            var engine = builder.Build();
+
+            var validResult = engine.Validate(password, PasswordValidationRule);
+
+            validResult.HasError().Should().Be(!isValid);
+        }
+    }
+}
