d42ea0d5177b024a865c30072d5769420ea070e8
manisero <michalaniserowicz@gmail.com>
2017-11-02 22:29:07 +0100

    Added 'Validator with dependency' sample

---
+++ src/Manisero.YouShallNotPass.Samples/Custom_validations/Validator_with_dependency.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Manisero.YouShallNotPass.Core.ValidationDefinition;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Custom_validations
+{
+    public class Validator_with_dependency
+    {
+        // UserRepository
+
+        public interface IUserRepository
+        {
+            bool UsernameExists(string username);
+        }
+
+        public class UserRepository : IUserRepository
+        {
+            public bool UsernameExists(string username) => true;
+        }
+
+        // UniqueUsername validation
+
+        public class UniqueUsernameValidationRule : IValidationRule<string, EmptyValidationError>
+        {
+        }
+
+        public class UniqueUsernameValidator : IValidator<UniqueUsernameValidationRule, string, EmptyValidationError>
+        {
+            private readonly IUserRepository _userRepository;
+
+            public UniqueUsernameValidator(IUserRepository userRepository)
+            {
+                _userRepository = userRepository;
+            }
+
+            public EmptyValidationError Validate(string value, UniqueUsernameValidationRule rule, ValidationContext context)
+            {
+                var isDuplicate = _userRepository.UsernameExists(value);
+
+                return isDuplicate
+                    ? EmptyValidationError.Some
+                    : EmptyValidationError.None;
+            }
+        }
+
+        [Fact]
+        public void sample()
+        {
+            var builder = new ValidationEngineBuilder();
+            builder.RegisterValidatorFactory(() => new UniqueUsernameValidator(new UserRepository()));
+
+            var engine = builder.Build();
+
+            var rule = new UniqueUsernameValidationRule();
+
+            var validResult = engine.Validate("user1", rule);
+
+            validResult.HasError().Should().BeTrue();
+        }
+    }
+}
