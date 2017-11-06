7df39dfb85745bcaa1fa886e76312cb57d123c08
manisero <michalaniserowicz@gmail.com>
2017-11-06 23:17:59 +0100

    Started replacing ValidationContext.Data with ValidationData type

--- src/Manisero.YouShallNotPass/ValidationContext.cs
+++ src/Manisero.YouShallNotPass/ValidationContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.Extensions;
 
 namespace Manisero.YouShallNotPass
 {
@@ -7,5 +8,40 @@ namespace Manisero.YouShallNotPass
         public ISubvalidationEngine Engine { get; set; }
 
         public IDictionary<string, object> Data { get; set; } // TODO: Consider replacing with some convenient data container (with nice Add and GetValueOrDefault<TValue> api)
+    }
+
+    public class ValidationData
+    {
+        private readonly IDictionary<string, object> _data = new Dictionary<string, object>();
+
+        public void Put(string key, object item)
+        {
+            _data[key] = item;
+        }
+
+        public ValidationData Put<TItem>(string key, TItem item)
+        {
+            _data[key] = item;
+            return this;
+        }
+
+        public TItem GetItemOrDefault<TItem>(string key)
+        {
+            var item = _data.GetValueOrDefault(key);
+
+            return item is TItem
+                ? (TItem)item
+                : default(TItem);
+        }
+
+        public TItem? GetItemOrNull<TItem>(string key)
+            where TItem : struct
+        {
+            var item = _data.GetValueOrDefault(key);
+
+            return item is TItem
+                ? (TItem?)item
+                : null;
+        }
     }
 }
