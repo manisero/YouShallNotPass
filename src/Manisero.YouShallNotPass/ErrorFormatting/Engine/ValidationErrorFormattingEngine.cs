c706efeaf7019c19395c4690e09ec9d59dc43a1d
manisero <michalaniserowicz@gmail.com>
2017-11-12 16:23:24 +0100

    Implemented ValidationErrorFormattingExecutor

--- src/Manisero.YouShallNotPass/ErrorFormatting/Engine/ValidationErrorFormattingEngine.cs
+++ src/Manisero.YouShallNotPass/ErrorFormatting/Engine/ValidationErrorFormattingEngine.cs
@@ -1,19 +1,33 @@
-﻿namespace Manisero.YouShallNotPass.ErrorFormatting.Engine
+﻿using System;
+
+namespace Manisero.YouShallNotPass.ErrorFormatting.Engine
 {
     public class ValidationErrorFormattingEngine<TFormat> : IValidationErrorFormattingEngine<TFormat>
     {
+        private readonly IValidationErrorFormattingExecutor<TFormat> _validationErrorFormattingExecutor;
+
+        public ValidationErrorFormattingEngine(
+            IValidationErrorFormattingExecutor<TFormat> validationErrorFormattingExecutor)
+        {
+            _validationErrorFormattingExecutor = validationErrorFormattingExecutor;
+        }
+
         public TFormat Format(IValidationResult validationResult)
         {
-            // TODO
-            return default(TFormat);
+            throw new NotImplementedException();
         }
 
         public TFormat Format<TRule, TValue, TError>(ValidationResult<TRule, TValue, TError> validationResult)
             where TRule : IValidationRule<TValue, TError>
             where TError : class
         {
-            // TODO
-            return default(TFormat);
+            // TODO: Avoid creating new context instance for each formatting
+            var context = new ValidationErrorFormattingContext<TFormat>
+            {
+                FormattingEngine = this
+            };
+
+            return _validationErrorFormattingExecutor.Execute(validationResult, context);
         }
     }
 }
---
+++ src/Manisero.YouShallNotPass/ErrorFormatting/Engine/ValidationErrorFormattingExecutor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Manisero.YouShallNotPass.Core.RuleKeyedOperationRegistration;
+
+namespace Manisero.YouShallNotPass.ErrorFormatting.Engine
+{
+    public interface IValidationErrorFormattingExecutor<TFormat>
+    {
+        TFormat Execute<TRule, TValue, TError>(
+            ValidationResult<TRule, TValue, TError> validationResult,
+            ValidationErrorFormattingContext<TFormat> context)
+            where TRule : IValidationRule<TValue, TError>
+            where TError : class;
+    }
+
+    public class ValidationErrorFormattingExecutor<TFormat> : IValidationErrorFormattingExecutor<TFormat>
+    {
+        private readonly IRuleKeyedOperationResolver<IValidationErrorFormatter<TFormat>> _formatterResolver;
+
+        public ValidationErrorFormattingExecutor(
+            IRuleKeyedOperationResolver<IValidationErrorFormatter<TFormat>> formatterResolver)
+        {
+            _formatterResolver = formatterResolver;
+        }
+
+        public TFormat Execute<TRule, TValue, TError>(
+            ValidationResult<TRule, TValue, TError> validationResult,
+            ValidationErrorFormattingContext<TFormat> context)
+            where TRule : IValidationRule<TValue, TError>
+            where TError : class
+        {
+            var untypedFormatter = _formatterResolver.TryResolve<TRule, TValue, TError>();
+
+            if (untypedFormatter == null)
+            {
+                throw new InvalidOperationException($"Unable to find formatter for result of validation of value '{typeof(TValue)}' against rule '{typeof(TRule)}'.");
+            }
+
+            var formatter = (IValidationErrorFormatter<TRule, TValue, TError, TFormat>)untypedFormatter;
+
+            return formatter.Format(validationResult, context);
+        }
+    }
+}
