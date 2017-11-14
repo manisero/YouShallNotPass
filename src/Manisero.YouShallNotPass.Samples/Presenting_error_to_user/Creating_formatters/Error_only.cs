c850375f09c4c8bd619743e0bf51bdd91448c649
manisero <michalaniserowicz@gmail.com>
2017-11-14 22:48:13 +0100

    Organizing formatters creation samples

---
+++ src/Manisero.YouShallNotPass.Samples/Presenting_error_to_user/Creating_formatters/Error_only.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using FluentAssertions;
+using Manisero.YouShallNotPass.ErrorFormatting;
+using Manisero.YouShallNotPass.Validations;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Presenting_error_to_user.Creating_formatters
+{
+    public class Error_only
+    {
+        public const string ErrorMessage = "Value should be an e-mail address.";
+
+        public static readonly ValidationResult<EmailValidationRule, string, EmailValidationError> ValidationResult
+            = new ValidationResult<EmailValidationRule, string, EmailValidationError>();
+
+        [Fact]
+        public void error_only_formatter_func()
+        {
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            formattingEngineBuilder.RegisterErrorOnlyFormatterFunc<EmailValidationError>(_ => ErrorMessage);
+
+            var formattingEngine = formattingEngineBuilder.Build();
+            var error = formattingEngine.Format(ValidationResult);
+
+            error.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void error_only_formatter_func_factory()
+        {
+            Func<EmailValidationError, string> formatter = _ => "Value should be an e-mail address.";
+
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            formattingEngineBuilder.RegisterErrorOnlyFormatterFuncFactory(() => formatter);
+
+            var formattingEngine = formattingEngineBuilder.Build();
+            var error = formattingEngine.Format(ValidationResult);
+
+            error.Should().NotBeNull();
+        }
+
+        public class EmailValidationErrorFormatter : IValidationErrorFormatter<EmailValidationError, string>
+        {
+            public string Format(EmailValidationError error, ValidationErrorFormattingContext<string> context)
+            {
+                return "Value should be an e-mail address.";
+            }
+        }
+
+        [Fact]
+        public void error_only_formatter()
+        {
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            formattingEngineBuilder.RegisterErrorOnlyFormatter(new EmailValidationErrorFormatter());
+
+            var formattingEngine = formattingEngineBuilder.Build();
+            var error = formattingEngine.Format(ValidationResult);
+
+            error.Should().NotBeNull();
+        }
+    }
+}
---
+++ src/Manisero.YouShallNotPass.Samples/Presenting_error_to_user/Creating_formatters/Error_rule_and_value.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Manisero.YouShallNotPass.ErrorFormatting;
+using Manisero.YouShallNotPass.Validations;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Presenting_error_to_user.Creating_formatters
+{
+    public class Error_rule_and_value
+    {
+        public const string ErrorMessageTemplate = "Value has {0} characters(s), while should have at least {1}.";
+
+        public static readonly ValidationResult<MinLengthValidationRule, string, MinLengthValidationError> ValidationResult
+            = new ValidationResult<MinLengthValidationRule, string, MinLengthValidationError>
+            {
+                Rule = new MinLengthValidationRule { MinLength = 2 },
+                Value = "a"
+            };
+
+        [Fact]
+        public void error_rule_and_value_formatter_func()
+        {
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            formattingEngineBuilder.RegisterErrorRuleAndValueFormatterFunc<MinLengthValidationRule, string, MinLengthValidationError>(
+                (e, r, v) => string.Format(ErrorMessageTemplate, v.Length, r.MinLength));
+
+            var formattingEngine = formattingEngineBuilder.Build();
+            var error = formattingEngine.Format(ValidationResult);
+
+            error.Should().NotBeNull();
+        }
+    }
+}
