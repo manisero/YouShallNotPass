7b8db865d3f4b3d8d9e630068afebba861517cd4
manisero <michalaniserowicz@gmail.com>
2017-11-19 15:50:35 +0100

    Removed unused ValidatorInterfaceConstants

--- src/Manisero.YouShallNotPass/Validator.cs
+++ src/Manisero.YouShallNotPass/Validator.cs
@@ -19,11 +19,4 @@ namespace Manisero.YouShallNotPass
     {
         Task<TError> ValidateAsync(TValue value, TRule rule, ValidationContext context);
     }
-
-    public static class ValidatorInterfaceConstants
-    {
-        public const int TRuleTypeParameterPosition = 0;
-        public const int TValueTypeParameterPosition = 1;
-        public const int TErrorTypeParameterPosition = 2;
-    }
 }
