5af7acf62562bbdd318c3edb42f6d267500fd3c8
manisero <michalaniserowicz@gmail.com>
2017-11-19 13:40:34 +0100

    Finished testing error only generic formatters registration

--- src/Manisero.YouShallNotPass.Samples/Presenting_error_to_user/Creating_formatters/Error_only_generic.cs
+++ src/Manisero.YouShallNotPass.Samples/Presenting_error_to_user/Creating_formatters/Error_only_generic.cs
@@ -16,40 +16,92 @@ namespace Manisero.YouShallNotPass.Samples.Presenting_error_to_user.Creating_for
                 Error = new MinValidationError<int> { MinValue = 1 }
             };
 
-        public class MinValidationErrorFormatter<TValue> : IValidationErrorFormatter<MinValidationError<TValue>, string>
+        public class Error
+        {
+            public Guid FormatterId { get; set; }
+            public string Message { get; set; }
+        }
+
+        public class MinValidationErrorFormatter<TValue> : IValidationErrorFormatter<MinValidationError<TValue>, Error>
             where TValue : IComparable<TValue>
         {
-            public string Format(
+            private readonly Guid _id = Guid.NewGuid();
+
+            public Error Format(
                 MinValidationError<TValue> error,
-                ValidationErrorFormattingContext<string> context)
-                => $"Value should be at least {error.MinValue}.";
+                ValidationErrorFormattingContext<Error> context)
+                => new Error
+                {
+                    FormatterId = _id,
+                    Message = $"Value should be at least {error.MinValue}."
+                };
         }
 
         [Fact]
-        public void error_only_generic_formatter()
+        public void error_only_generic_formatter_singleton()
         {
-            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<Error>();
             formattingEngineBuilder.RegisterErrorOnlyGenericFormatter(typeof(MinValidationErrorFormatter<>));
 
             var formattingEngine = formattingEngineBuilder.Build();
-            var error = formattingEngine.Format(ValidationResult);
 
-            error.Should().NotBeNull();
+            var error1 = formattingEngine.Format(ValidationResult);
+            error1.Should().NotBeNull();
+
+            var error2 = formattingEngine.Format(ValidationResult);
+            error2.FormatterId.Should().Be(error1.FormatterId);
         }
 
         [Fact]
-        public void error_only_generic_formatter_factory()
+        public void error_only_generic_formatter_per_resolve()
         {
-            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<string>();
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<Error>();
+            formattingEngineBuilder.RegisterErrorOnlyGenericFormatter(typeof(MinValidationErrorFormatter<>), false);
+
+            var formattingEngine = formattingEngineBuilder.Build();
+
+            var error1 = formattingEngine.Format(ValidationResult);
+            error1.Should().NotBeNull();
+
+            var error2 = formattingEngine.Format(ValidationResult);
+            error2.FormatterId.Should().NotBe(error1.FormatterId);
+        }
+
+        [Fact]
+        public void error_only_generic_formatter_factory_singleton()
+        {
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<Error>();
 
             // You may want to replace below lambda with your DI Container usage
             formattingEngineBuilder.RegisterErrorOnlyGenericFormatterFactory(typeof(MinValidationErrorFormatter<>),
-                                                                             type => (IValidationErrorFormatter<string>)Activator.CreateInstance(type));
+                                                                             type => (IValidationErrorFormatter<Error>)Activator.CreateInstance(type));
 
             var formattingEngine = formattingEngineBuilder.Build();
-            var error = formattingEngine.Format(ValidationResult);
 
-            error.Should().NotBeNull();
+            var error1 = formattingEngine.Format(ValidationResult);
+            error1.Should().NotBeNull();
+
+            var error2 = formattingEngine.Format(ValidationResult);
+            error2.FormatterId.Should().Be(error1.FormatterId);
+        }
+
+        [Fact]
+        public void error_only_generic_formatter_factory_per_resolve()
+        {
+            var formattingEngineBuilder = new ValidationErrorFormattingEngineBuilder<Error>();
+
+            // You may want to replace below lambda with your DI Container usage
+            formattingEngineBuilder.RegisterErrorOnlyGenericFormatterFactory(typeof(MinValidationErrorFormatter<>),
+                                                                             type => (IValidationErrorFormatter<Error>)Activator.CreateInstance(type),
+                                                                             false);
+
+            var formattingEngine = formattingEngineBuilder.Build();
+
+            var error1 = formattingEngine.Format(ValidationResult);
+            error1.Should().NotBeNull();
+
+            var error2 = formattingEngine.Format(ValidationResult);
+            error2.FormatterId.Should().NotBe(error1.FormatterId);
         }
     }
 }
