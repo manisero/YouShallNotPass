bc53d72c68c9f749c64f467166d1ff6d810b6e54
manisero <michalaniserowicz@gmail.com>
2017-11-20 22:31:12 +0100

    Started implementing rules registration

---
+++ src/Manisero.YouShallNotPass.Samples/Registering_rules/Sample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.Validations;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.Samples.Registering_rules
+{
+    public class Sample
+    {
+        public class CreateUserCommand
+        {
+            public int UserId { get; set; }
+        }
+
+        public static readonly ComplexValidationRule<CreateUserCommand> Rule = new ComplexValidationRule<CreateUserCommand>
+        {
+            MemberRules = new Dictionary<string, IValidationRule>
+            {
+                [nameof(CreateUserCommand.UserId)] = new MinValidationRule<int> { MinValue = 1 }
+            }
+        };
+
+        [Fact]
+        public void sample()
+        {
+            var engineBuilder = new ValidationEngineBuilder();
+            var engine = engineBuilder.Build();
+
+            //engine.RegisterRule(Rule);
+        }
+    }
+}
