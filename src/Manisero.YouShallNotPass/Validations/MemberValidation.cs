1718fcb3d66c6adddaca4673f3bc71c888e24096
manisero <michalaniserowicz@gmail.com>
2017-11-26 17:06:24 +0100

    Added MemberValidation

---
+++ src/Manisero.YouShallNotPass/Validations/MemberValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Manisero.YouShallNotPass.Validations
+{
+    public class MemberValidationRule<TOwner, TValue> : IValidationRule<TOwner, MemberValidationError>
+    {
+        public string MemberName { get; set; }
+
+        public Func<TOwner, TValue> ValueGetter { get; set; }
+
+        public IValidationRule<TValue> ValueValidationRule { get; set; }
+    }
+
+    public class MemberValidationError
+    {
+        public IValidationResult Violation { get; set; }
+    }
+
+    public class PropertyValidator<TOwner, TValue> : IValidator<MemberValidationRule<TOwner, TValue>, TOwner, MemberValidationError>
+    {
+        public MemberValidationError Validate(TOwner value, MemberValidationRule<TOwner, TValue> rule, ValidationContext context)
+        {
+            var memberValue = rule.ValueGetter(value);
+            var validationResult = context.Engine.Validate(memberValue, rule.ValueValidationRule);
+
+            return validationResult.HasError()
+                ? new MemberValidationError { Violation = validationResult }
+                : null;
+        }
+    }
+}
