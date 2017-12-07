525da97d4af910464feaf221147dbb18f6ab28d1
manisero <michalaniserowicz@gmail.com>
2017-12-07 23:32:36 +0100

    Added IsEnumValue and Null validations

---
+++ src/Manisero.YouShallNotPass/Validations/IsEnumValueValidation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Manisero.YouShallNotPass.Validations;
+
+namespace Manisero.YouShallNotPass.Validations
+{
+    public static class IsEnumValueValidation
+    {
+        public class Rule<TEnum> : IValidationRule<TEnum, Error>
+            where TEnum : struct
+        {
+        }
+
+        public class Error
+        {
+            public static readonly Error Instance = new Error();
+        }
+
+        public class Validator<TEnum> : IValidator<Rule<TEnum>, TEnum, Error>
+            where TEnum : struct
+        {
+            public Error Validate(TEnum value, Rule<TEnum> rule, ValidationContext context)
+            {
+                return Enum.IsDefined(typeof(TEnum), value)
+                    ? null
+                    : Error.Instance;
+            }
+        }
+
+        public static Rule<TEnum> IsEnumValue<TEnum>(this ValidationRuleBuilder<TEnum> builder)
+            where TEnum : struct
+            => new Rule<TEnum>();
+    }
+}
+
+namespace Manisero.YouShallNotPass.Core.ValidatorRegistration
+{
+    internal static partial class DefaultValidatorsRegistrar
+    {
+        private static readonly Action<IValidationEngineBuilder> IsEnumValue
+            = x => x.RegisterFullGenericValidator(typeof(IsEnumValueValidation.Validator<>));
+    }
+}
---
+++ src/Manisero.YouShallNotPass/Validations/NullValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Manisero.YouShallNotPass.Validations;
+
+namespace Manisero.YouShallNotPass.Validations
+{
+    public static class NullValidation
+    {
+        public class Rule<TValue> : IValidationRule<TValue, Error>
+        {
+        }
+
+        public class Error
+        {
+            public static readonly Error Instance = new Error();
+        }
+
+        public class Validator<TValue> : IValidator<Rule<TValue>, TValue, Error>
+        {
+            public Error Validate(TValue value, Rule<TValue> rule, ValidationContext context)
+            {
+                // No ValidatesNull attribute on rule, so value will never be null
+                return Error.Instance;
+            }
+        }
+
+        public static Rule<TValue> Null<TValue>(this ValidationRuleBuilder<TValue> builder)
+            => new Rule<TValue>();
+    }
+}
+
+namespace Manisero.YouShallNotPass.Core.ValidatorRegistration
+{
+    internal static partial class DefaultValidatorsRegistrar
+    {
+        private static readonly Action<IValidationEngineBuilder> Null
+            = x => x.RegisterFullGenericValidator(typeof(NullValidation.Validator<>));
+    }
+}
