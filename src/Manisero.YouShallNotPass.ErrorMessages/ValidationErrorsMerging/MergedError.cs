50c99613fd5dea5e9a6671deab2ca744a6e4fc1c
manisero <michalaniserowicz@gmail.com>
2017-12-09 10:49:25 +0100

    Moved MergedError to separate file

---
+++ src/Manisero.YouShallNotPass.ErrorMessages/ValidationErrorsMerging/MergedError.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.ErrorMessages.Formatters;
+using Manisero.YouShallNotPass.ErrorMessages.Utils;
+
+namespace Manisero.YouShallNotPass.ErrorMessages.ValidationErrorsMerging
+{
+    internal class MergedError
+    {
+        private readonly ICollection<IValidationErrorMessage> _selfErrors = new List<IValidationErrorMessage>();
+        private readonly IDictionary<string, MergedError> _memberErrors = new Dictionary<string, MergedError>();
+        private readonly IDictionary<int, MergedError> _itemErrors = new Dictionary<int, MergedError>();
+
+        public void Add(IValidationErrorMessage error)
+        {
+            _selfErrors.Add(error);
+        }
+
+        public MergedError GetMemberError(string memberName)
+        {
+            return _memberErrors.GetOrAdd(memberName, _ => new MergedError());
+        }
+
+        public MergedError GetItemError(int itemIndex)
+        {
+            return _itemErrors.GetOrAdd(itemIndex, _ => new MergedError());
+        }
+
+        public ICollection<IValidationErrorMessage> ToErrorMessages()
+        {
+            var result = new List<IValidationErrorMessage>(_selfErrors);
+
+            foreach (var memberNameToError in _memberErrors)
+            {
+                result.Add(new MemberValidationErrorMessage
+                {
+                    MemberName = memberNameToError.Key,
+                    Errors = memberNameToError.Value.ToErrorMessages()
+                });
+            }
+
+            foreach (var itemIndexToError in _itemErrors)
+            {
+                result.Add(new ItemValidationErrorMessage
+                {
+                    ItemIndex = itemIndexToError.Key,
+                    Errors = itemIndexToError.Value.ToErrorMessages()
+                });
+            }
+
+            return result;
+        }
+    }
+}
--- src/Manisero.YouShallNotPass.ErrorMessages/ValidationErrorsMerging/ValidationErrorsMerger.cs
+++ src/Manisero.YouShallNotPass.ErrorMessages/ValidationErrorsMerging/ValidationErrorsMerger.cs
@@ -11,53 +11,6 @@ namespace Manisero.YouShallNotPass.ErrorMessages.ValidationErrorsMerging
 
     internal class ValidationErrorsMerger : IValidationErrorsMerger
     {
-        private class MergedError
-        {
-            private readonly ICollection<IValidationErrorMessage> _selfErrors = new List<IValidationErrorMessage>();
-            private readonly IDictionary<string, MergedError> _memberErrors = new Dictionary<string, MergedError>();
-            private readonly IDictionary<int, MergedError> _itemErrors = new Dictionary<int, MergedError>();
-
-            public void Add(IValidationErrorMessage error)
-            {
-                _selfErrors.Add(error);
-            }
-
-            public MergedError GetMemberError(string memberName)
-            {
-                return _memberErrors.GetOrAdd(memberName, _ => new MergedError());
-            }
-
-            public MergedError GetItemError(int itemIndex)
-            {
-                return _itemErrors.GetOrAdd(itemIndex, _ => new MergedError());
-            }
-
-            public ICollection<IValidationErrorMessage> ToErrorMessages()
-            {
-                var result = new List<IValidationErrorMessage>(_selfErrors);
-
-                foreach (var memberNameToError in _memberErrors)
-                {
-                    result.Add(new MemberValidationErrorMessage
-                    {
-                        MemberName = memberNameToError.Key,
-                        Errors = memberNameToError.Value.ToErrorMessages()
-                    });
-                }
-
-                foreach (var itemIndexToError in _itemErrors)
-                {
-                    result.Add(new ItemValidationErrorMessage
-                    {
-                        ItemIndex = itemIndexToError.Key,
-                        Errors = itemIndexToError.Value.ToErrorMessages()
-                    });
-                }
-
-                return result;
-            }
-        }
-
         public ICollection<IValidationErrorMessage> Merge(ICollection<IValidationErrorMessage> errors)
         {
             var mergedError = new MergedError();
@@ -73,6 +26,7 @@ namespace Manisero.YouShallNotPass.ErrorMessages.ValidationErrorsMerging
                 if (errorMessage.Code.EqualsOrdinalIgnoreCase(ErrorCodes.Member))
                 {
                     var memberErrorMessage = (MemberValidationErrorMessage)errorMessage;
+
                     var memberError = mergedErrorToFill.GetMemberError(memberErrorMessage.MemberName);
                     MergeInto(memberErrorMessage.Errors, memberError);
                 }
