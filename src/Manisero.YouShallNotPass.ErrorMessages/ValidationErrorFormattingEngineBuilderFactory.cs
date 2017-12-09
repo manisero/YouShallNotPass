1408306e1c71cbacb4ea54ca53f278aecd6f3675
manisero <michalaniserowicz@gmail.com>
2017-12-09 10:57:06 +0100

    Started implementing ValidationErrorFormattingEngineBuilderFactory

---
+++ src/Manisero.YouShallNotPass.ErrorMessages/ValidationErrorFormattingEngineBuilderFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.ErrorFormatting;
+
+namespace Manisero.YouShallNotPass.ErrorMessages
+{
+    public interface IValidationErrorFormattingEngineBuilderFactory
+    {
+        IValidationErrorFormattingEngineBuilder<ICollection<IValidationErrorMessage>> Create(
+            bool registerDefaultFormatters = true);
+    }
+
+    public class ValidationErrorFormattingEngineBuilderFactory : IValidationErrorFormattingEngineBuilderFactory
+    {
+        public IValidationErrorFormattingEngineBuilder<ICollection<IValidationErrorMessage>> Create(
+            bool registerDefaultFormatters = true)
+        {
+            var builder = new ValidationErrorFormattingEngineBuilder<ICollection<IValidationErrorMessage>>();
+
+            if (registerDefaultFormatters)
+            {
+                // TODO: Register
+            }
+
+            return builder;
+        }
+    }
+}
