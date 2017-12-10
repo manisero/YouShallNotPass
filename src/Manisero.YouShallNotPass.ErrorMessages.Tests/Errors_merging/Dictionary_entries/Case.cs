1d6c7c77941759923ad8db75d3156780d2fdc41e
manisero <michalaniserowicz@gmail.com>
2017-12-11 00:07:18 +0100

    Started implementing dictionary entries errors merging

---
+++ src/Manisero.YouShallNotPass.ErrorMessages.Tests/Errors_merging/Dictionary_entries/Case.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.Validations;
+
+namespace Manisero.YouShallNotPass.ErrorMessages.Tests.Errors_merging.Dictionary_entries
+{
+    public class Case
+    {
+        public class UpdateEmailsCommand
+        {
+            public IDictionary<int, string> UserIdToEmail { get; set; }
+        }
+
+        public static IValidationRule<UpdateEmailsCommand> Rule = new ValidationRuleBuilder<UpdateEmailsCommand>()
+            .All(b => b.Member(
+                     x => x.UserIdToEmail,
+                     b1 => b1.All(
+                         b2 => b2.Dictionary(b3 => b3.MinLength(5)),
+                         b2 => b2.Dictionary(b3 => b3.Email()))));
+    }
+}
---
+++ src/Manisero.YouShallNotPass.ErrorMessages.Tests/Errors_merging/Dictionary_entries/Samples.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Manisero.YouShallNotPass.ErrorMessages.Formatters;
+using Xunit;
+
+namespace Manisero.YouShallNotPass.ErrorMessages.Tests.Errors_merging.Dictionary_entries
+{
+    public class Samples
+    {
+        static Samples()
+        {
+            AssertionOptions.AssertEquivalencyUsing(c => c.RespectingRuntimeTypes());
+        }
+
+        private IValidationFacade CreateValidationFacade()
+        {
+            var validationEngine = new ValidationEngineBuilder().Build();
+            var formattingEngine = new ValidationErrorFormattingEngineBuilderFactory().Create().Build();
+
+            return new ValidationFacade(validationEngine, formattingEngine);
+        }
+
+        [Fact]
+        public void errors_regarding_same_entry_are_merged()
+        {
+            var validationFacade = CreateValidationFacade();
+
+            var command = new Case.UpdateEmailsCommand
+            {
+                UserIdToEmail = new Dictionary<int, string> { [1] = "a" }
+            };
+
+            var error = validationFacade.Validate(command, Case.Rule);
+            error.Should().NotBeNull("Validation is expected to fail.");
+
+            error.ShouldBeEquivalentTo(new[]
+            {
+                new MemberValidationErrorMessage
+                {
+                    MemberName = nameof(Case.UpdateEmailsCommand.UserIdToEmail),
+                    Errors = new IValidationErrorMessage[]
+                    {
+                        new EntryValidationErrorMessage
+                        {
+                            EntryKey = 1,
+                            Errors = new IValidationErrorMessage[]
+                            {
+                                new MinLengthValidationErrorMessage { MinLength = 5 },
+                                new ValidationErrorMessage { Code = ErrorCodes.Email }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
