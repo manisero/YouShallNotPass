3635f24f8f1399dbff97139a5347a21a3bfe63ef
manisero <michalaniserowicz@gmail.com>
2017-12-10 17:10:21 +0100

    Created failing test for errors merging

--- src/Manisero.YouShallNotPass.ErrorMessages.Tests/Errors_merging/Sample.cs
+++ src/Manisero.YouShallNotPass.ErrorMessages.Tests/Errors_merging/Sample.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using FluentAssertions;
+using Manisero.YouShallNotPass.ErrorMessages.Formatters;
 using Manisero.YouShallNotPass.Validations;
 using Xunit;
 
@@ -14,10 +14,8 @@ namespace Manisero.YouShallNotPass.ErrorMessages.Tests.Errors_merging
         }
 
         public static IValidationRule<CreateUserCommand> Rule = new ValidationRuleBuilder<CreateUserCommand>()
-            .Member(x => x.Email,
-                    b => b.All(
-                        b1 => b1.MinLength(3),
-                        b1 => b1.Email()));
+            .All(b => b.Member(x => x.Email, b1 => b1.MinLength(3)),
+                 b => b.Member(x => x.Email, b1 => b1.Email()));
 
         public static CreateUserCommand Command = new CreateUserCommand
         {
@@ -30,11 +28,24 @@ namespace Manisero.YouShallNotPass.ErrorMessages.Tests.Errors_merging
             var validationEngine = new ValidationEngineBuilder().Build();
             var formattingEngine = new ValidationErrorFormattingEngineBuilderFactory().Create().Build();
 
-            var validationResult = validationEngine.Validate(Command);
+            var validationResult = validationEngine.Validate(Command, Rule);
 
             validationResult.HasError().Should().BeTrue("Validation is expected to fail.");
 
             var errorMessage = formattingEngine.Format(validationResult).ToArray();
+
+            errorMessage.ShouldBeEquivalentTo(new[]
+            {
+                new MemberValidationErrorMessage
+                {
+                    MemberName = nameof(CreateUserCommand.Email),
+                    Errors = new IValidationErrorMessage[]
+                    {
+                        new MinLengthValidationErrorMessage { MinLength = 3 },
+                        new ValidationErrorMessage { Code = ErrorCodes.Email }
+                    }
+                }
+            });
         }
     }
 }
